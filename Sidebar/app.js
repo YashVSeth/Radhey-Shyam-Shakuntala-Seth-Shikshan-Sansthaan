2f94f9509436046b7a02e1703965aed4117e8a8c
YashVSeth <yash29seth@gmail.com>
2026-01-13 02:14:14 +0530

    Add files via upload

---
+++ Sidebar/app.js
@@ -0,0 +1,43 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
+
+function toggleSubMenu(button){
+
+  if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+  }
+
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+
+  if(sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+
+function closeAllSubMenus(){
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
+}
+function loadPage(page){
+  fetch(page)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('content-area').innerHTML = data;
+    });
+}
+
+// auto load home page when site opens
+document.addEventListener('DOMContentLoaded', () => {
+  loadPage('home.html');
+});
